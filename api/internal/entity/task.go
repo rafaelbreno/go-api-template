1db1d5ebfa80aed0ff2bf1de79f0d1fdf9e2bd66
Rafael Breno <rafael_breno@outlook.com>
2021-04-26 23:19:16 -0300

    feat: implement states into entity

--- api/internal/entity/task.go
+++ api/internal/entity/task.go
@@ -1,16 +1,20 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/rafaelbreno/go-api-template/api/internal/states"
+)
 
 type Task struct {
 	ID          uint   `db:"id" json:"task_id"`              // ID auto-incremented by DB
 	Title       string `db:"title" json:"title"`             // Title, required
 	Description string `db:"description" json:"description"` // Description, not required
 
-	// 0 - Incomplete
-	// 1 - Finished
-	// 2 - Cancelled
-	Status uint8 `db:"status" json:"status"`
+	// 0 - TaskIncomplete
+	// 1 - TaskComplete
+	// 2 - TaskCancelled
+	Status states.TaskStatus `db:"status" json:"status"`
 
 	// When task were created
 	// auto-generated
