dd95f436117d17be21dce1be0b8ab4d3d31ace8f
Rafael Breno <rafael_breno@outlook.com>
2021-05-24 17:29:05 -0300

    feat: Add relationship One(list) to Many(task)

--- services/api/internal/entity/list.go
+++ services/api/internal/entity/list.go
@@ -11,6 +11,8 @@ type List struct {
 	Title       string `db:"title" json:"title"`             // Title, required
 	Description string `db:"description" json:"description"` // Description, not required
 
+	Tasks []Task // Tasks assigned to this list
+
 	// 0 - ListNotStarted ListStatus = iota
 	// 1 - ListInProgress
 	// 2 - ListCompleted
--- services/api/internal/entity/task.go
+++ services/api/internal/entity/task.go
@@ -10,6 +10,8 @@ type Task struct {
 	Title       string `db:"title" json:"title"`             // Title, required
 	Description string `db:"description" json:"description"` // Description, not required
 
+	ListID uint `db:"list_id" json:"list_id"` // Foreign Key
+
 	// 0 - TaskIncomplete
 	// 1 - TaskComplete
 	// 2 - TaskCancelled
