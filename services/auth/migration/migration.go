72d90257e02d8e9e8a1b090a753caa29c7e0e20c
Rafael Breno <rafael_breno@outlook.com>
2021-05-26 19:44:48 -0300

    feat: Add migration messages

--- services/auth/migration/migration.go
+++ services/auth/migration/migration.go
@@ -1,12 +1,17 @@
 package migration
 
 import (
+	"fmt"
+
 	"github.com/rafaelbreno/go-api-template/services/auth/config"
 	"github.com/rafaelbreno/go-api-template/services/auth/entity"
 )
 
 func init() {
+	fmt.Println("[Migrations] Running...")
 	config.
 		DB.
 		AutoMigrate(entity.User{})
+
+	fmt.Println("[Migrations] Done!")
 }
