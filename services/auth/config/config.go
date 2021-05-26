5258efd764184feeb5b15bef6faf326b7e07fc3d
Rafael Breno <rafael_breno@outlook.com>
2021-05-26 17:53:48 -0300

    feat: Add database connection

---
+++ services/auth/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	DATABASE_URL = "postgresql://%s:%s@%s:%s/%s"
+)
+
+var DB *gorm.DB
+
+func init() {
+	var err error
+
+	DB, err = gorm.Open(postgres.Open(mountDatabaseURL()), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mountDatabaseURL() string {
+	return fmt.Sprintf(
+		DATABASE_URL,
+		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+}
