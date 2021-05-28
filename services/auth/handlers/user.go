e7efb953c46c2e63f51d4fedbc8d0b7c91cb94fa
Rafael Breno <rafael_breno@outlook.com>
2021-05-28 20:04:57 -0300

    feat: Add create user handler

---
+++ services/auth/handlers/user.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/rafaelbreno/go-api-template/services/auth/entity"
+	"github.com/rafaelbreno/go-api-template/services/auth/repository"
+)
+
+type userHandler struct {
+	repo repository.UserRepositoryDB
+}
+
+func NewUserHandler() userHandler {
+	return userHandler{repository.NewUserRepositoryDB()}
+}
+
+func (u userHandler) Create(c *fiber.Ctx) error {
+	var user entity.User
+
+	if err := c.BodyParser(&user); err != nil {
+		c.
+			Status(http.StatusServiceUnavailable).
+			JSON(map[string]string{
+				"message": err.Error(),
+			})
+		return err
+	}
+
+	user, err := u.repo.Create(user)
+	if err != nil {
+		c.
+			Status(http.StatusUnprocessableEntity).
+			JSON(map[string]string{
+				"message": err.Error(),
+			})
+		return err
+	}
+
+	c.
+		Status(http.StatusUnprocessableEntity).
+		JSON(user)
+	return nil
+}
