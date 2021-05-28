fafeb67a65fbf3f155582c40f75c21748139ca5c
Rafael Breno <rafael_breno@outlook.com>
2021-05-28 20:35:30 -0300

    fix: Add gorm fields tags

--- services/auth/entity/user.go
+++ services/auth/entity/user.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `db:"username" json:"username"`
-	Password string `db:"password" json:"password"`
+	Username string `db:"username" json:"username" gorm:"varchar(128);unique;not null"`
+	Password string `db:"password" json:"password" gorm:"varchar(128);not null"`
 }
